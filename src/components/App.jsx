4ce980c2d8c9f337fe4acfe42eb88763f6dd4f6a
Rithvik Poojary <68432252+rithvikgit@users.noreply.github.com>
2022-03-20 02:09:16 +0530

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
-function App(){
-    return <div>
-        <Header/>
-        <Note/>
-        <Footer/>
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(noteItem => 
+          <Note
+            key={noteItem}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        )}
+      <Footer />
     </div>
+  );
 }
 
 export default App;
